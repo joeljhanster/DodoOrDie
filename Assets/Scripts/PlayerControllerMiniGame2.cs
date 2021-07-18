1f2d6e7c48e28fe0981f22840037a348bb2a1142
Dev Bahl <bahldev@gmail.com>
2021-07-18 20:20:24 +0800

    mini game 2

--- Assets/Scripts/PlayerControllerMiniGame2.cs
+++ Assets/Scripts/PlayerControllerMiniGame2.cs
@@ -1,20 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerMiniGame2 : MonoBehaviour
 {
 
     public float rockEffect = 10;
 
     private Rigidbody2D dodoBody;
     private Animator dodoAnimator;
-
-    public float maxSpeed;
-    public float speed;
+    private AudioSource dodoAudio;
+    private bool faceRightState = true;
+    private bool onGroundState = true;
+    public float maxSpeed = 20;
+    public float speed = 30;
+    public float upForce = 20;
 
     private float moveLeft;
     private float moveRight;
@@ -60,13 +63,34 @@ public class PlayerController : MonoBehaviour
         Application.targetFrameRate =  30;
         dodoBody = GetComponent<Rigidbody2D>();
         dodoAnimator = GetComponent<Animator>();
+        dodoAudio = GetComponent<AudioSource>();
     }
 
 
     // FixedUpdate may be called once per frame. See documentation for details.
     void FixedUpdate(){
-        Vector2 direction = new Vector2(moveRight - moveLeft, moveUp - moveDown);
-        dodoBody.AddForce(direction * speed); 
+
+        // dynamic rigidbody
+        // float moveHorizontal = Input.GetAxis("Horizontal");
+        // Debug.Log("moveRight: "+ moveRight);
+        // Debug.Log("moveLeft: "+ moveLeft);
+        // Debug.Log(moveRight - moveLeft);
+        Vector2 horizontalDirection = new Vector2(moveRight - moveLeft, 0);
+        if (dodoBody.velocity.magnitude < maxSpeed){
+            dodoBody.AddForce(horizontalDirection * speed); 
+        }
+        
+        if(dodoBody.velocity.magnitude == 0){
+            dodoAnimator.SetBool("moveRight", false);
+            dodoAnimator.SetBool("moveLeft", false);
+            dodoAnimator.SetBool("moveUp", false);
+        }
+        if ((jump > 0) && onGroundState)
+        {
+            PlayJumpSound();
+            dodoBody.AddForce(Vector2.up * upForce, ForceMode2D.Impulse);
+            onGroundState = false;
+        }
     }
 
     void OnTriggerEnter2D(Collision2D col)
@@ -75,6 +99,7 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Player eaten by eagle!");
         }
+        
     }
 
     void OnCollisionEnter2D(Collision2D col)
@@ -84,29 +109,38 @@ public class PlayerController : MonoBehaviour
             dodoBody.AddForce(Vector2.up * rockEffect, ForceMode2D.Impulse);
         }
 
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Collision with Ground!");
+            onGroundState = true; // back on ground
+        }
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = new Vector2(moveRight - moveLeft, moveUp - moveDown);
-        Debug.Log(direction);
-        // dodoBody.MovePosition(dodoBody.position + speed * direction * Time.fixedDeltaTime);
-        if (moveRight > 0) {
-            dodoAnimator.SetBool("moveRight", true);
-            dodoAnimator.SetBool("moveLeft", false);
-        } else if (moveLeft > 0) {
+
+        // toggle state
+        if ((moveLeft > moveRight) && (moveLeft >  0)){
+            // faceRightState = false;
             dodoAnimator.SetBool("moveRight", false);
             dodoAnimator.SetBool("moveLeft", true);
-        } else if (moveUp > 0) {
-            dodoAnimator.SetBool("moveRight", false);
-            dodoAnimator.SetBool("moveLeft", false);
-            dodoAnimator.SetBool("moveUp", true);
-        } else {
-            dodoAnimator.SetBool("moveRight", false);
+            dodoAnimator.SetBool("moveUp", false);
+            // marioSprite.flipX = true;
+        }
+
+        if ((moveRight > moveLeft) && (moveRight >  0)){
+            // faceRightState = true;
+            dodoAnimator.SetBool("moveRight", true);
             dodoAnimator.SetBool("moveLeft", false);
             dodoAnimator.SetBool("moveUp", false);
+            // marioSprite.flipX = false;
         }
+
+    }
+
+    void  PlayJumpSound(){
+	    dodoAudio.PlayOneShot(dodoAudio.clip);
     }
 }
