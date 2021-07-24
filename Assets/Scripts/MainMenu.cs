1c90f4f0eb40356703a15406fea9cec82859a1d9
Holy Cow <tscd.1996@gmail.com>
2021-07-25 01:26:07 +0800

    Add Eagle and Eagle 2

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class MainMenu : MonoBehaviour
+{
+    private float moveLeft;
+    private float moveRight;
+    private float moveUp;
+    private float moveDown;
+
+    private PlayerControls controls;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Gameplay.MoveUp.performed += ctx => moveUp = ctx.ReadValue<float>();
+        controls.Gameplay.MoveUp.canceled += ctx => moveUp = 0.0f;
+
+        controls.Gameplay.MoveDown.performed += ctx => moveDown = ctx.ReadValue<float>();
+        controls.Gameplay.MoveDown.canceled += ctx => moveDown = 0.0f;
+    }
+
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+    void  Start()
+    {
+        // Set to be 30 FPS
+        Application.targetFrameRate =  30;
+    }
+    void Update()
+    {
+        if (moveUp>0){
+            Debug.Log("pressed up");
+        }
+        else if (moveDown>0){
+           Debug.Log("pressed down");
+            
+        }
+    }
+
+
+   public void PlayGame(){
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+   }
+   public void QuitGame(){
+       Debug.Log("Quit game");
+       Application.Quit();
+   }
+}
