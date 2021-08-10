d97eded7bd13cee4f8dc236b34f170503bf00d5b
Mulie23 <47581954+Mulie23@users.noreply.github.com>
2021-08-10 21:00:53 +0800

    update

--- Assets/Scripts/game3/Border.cs
+++ Assets/Scripts/game3/Border.cs
@@ -2,9 +2,29 @@
 using System.Collections;
 
 public class Border : MonoBehaviour {
+    public Vector3 vector3;
+    public GameObject Player;
+    private AudioSource failaudio;
+    public UpdateScore other;
 
+    void Start(){
+        Player = GameObject.Find("Player");
+        failaudio = GetComponent<AudioSource>();
+
+    }
     void OnTriggerEnter2D(Collider2D coll) {
         if (coll.name == "Player")
-            Destroy(coll.gameObject);
+            respawn();
+    }
+
+    public void respawn(){
+        if (Player.transform.localPosition == vector3){
+            return;
+        }
+        else{
+            Player.transform.localPosition = vector3;
+            other.GetComponent<UpdateScore>().ifdie();
+            failaudio.PlayOneShot(failaudio.clip);
+        }
     }
 }
